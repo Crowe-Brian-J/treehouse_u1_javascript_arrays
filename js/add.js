8dc4e6fc272c95308c50bb50d2c15d4c34ee83ee
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-23 16:47:10 -0400

    add.js exercise

--- js/add.js
+++ js/add.js
@@ -1,1 +1,5 @@
 const instruments = ['piano', 'drums', 'trumpet']
+//push adds to end of array
+instruments.push('guitar', 'violin', 'triangle')
+//unshift adds to begginning of array
+instruments.unshift('cowbell', 'tuba')
