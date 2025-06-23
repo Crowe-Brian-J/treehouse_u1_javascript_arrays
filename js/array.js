411d2d9ec3f8a11525f1187078242768469d727f
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-23 16:31:13 -0400

    array.js exercise

---
+++ js/array.js
@@ -0,0 +1,8 @@
+//index value: first is 0, second is 1, etc.
+const planets = ['Mercury', 'Venus', 'Earth', 'Mars']
+
+console.log(planets[0])
+console.log(planets[1])
+console.log(planets[2])
+console.log(planets[3])
+console.log(planets[4]) //undefined because it doesn't exist
