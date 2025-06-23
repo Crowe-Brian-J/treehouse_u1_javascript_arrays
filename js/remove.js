83ecc377f287bd39b771ad95096fe220262fbc15
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-23 16:59:52 -0400

    remove.js exercise

---
+++ js/remove.js
@@ -0,0 +1,7 @@
+const shoppingList = ['bread', 'butter', 'honey', 'coffee']
+//pop removes last item
+let lastItem = shoppingList.pop() //coffee
+//shift removes first item
+let firstItem = shoppingList.shift() //bread
+console.log(shoppingList[0]) // butter
+//neither pop nor shift accepts argument
