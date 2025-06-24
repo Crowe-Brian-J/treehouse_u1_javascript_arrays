e53c9300dea419472453e43d3830d610c5167cb9
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-24 14:17:54 -0400

    music playlist-1 exercise

--- js/playlist-1.js
+++ js/playlist-1.js
@@ -6,3 +6,17 @@ const playlist = [
   'Three Little Birds',
   'The Way You Look Tonight'
 ]
+
+const createListItems = (arr) => {
+  let items = ''
+  for (let i = 0; i < arr.length; i++) {
+    items += `<li>${arr[i]}</li>`
+  }
+  return items
+}
+
+document.querySelector('main').innerHTML = `
+  <ol>
+    ${createListItems(playlist)}
+  </ol>
+`
