4ffe5b5ae3836fbfcfa81475142d3b89f9f777c7
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-24 15:01:35 -0400

    search-list.js exercise

--- js/search-list.js
+++ js/search-list.js
@@ -18,3 +18,11 @@ const inStock = [
 ]
 const search = prompt('Search for a product.')
 let message
+
+if (inStock.includes(search)) {
+  message = `Yes, we have <strong>${search}</strong>.`
+} else {
+  message = `Sorry, we do not have <strong>${search}</strong>.`
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`
