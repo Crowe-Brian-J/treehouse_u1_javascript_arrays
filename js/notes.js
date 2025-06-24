6666bb30993a8d2d80f3e9cb640e480d0b41f779
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-24 14:55:19 -0400

    notes.js for methods

---
+++ js/notes.js
@@ -0,0 +1,24 @@
+/*
+---Array Methods
+  * Add Elements
+    - .push() adds whatever is in the parenthesis to the end of the array
+    - .unshift() adds whatever is in the parenthesis to the beginning of the array
+
+  * Remove Elements
+    - .pop() removes whatever is at the end of the array. Accepts no arguments in parenthesis
+    - .shift() removes whatever is at the beginning of the array. Accepts no arguments...
+    -- If needed to store either of .pop() or .shift() values, declare variable, set equal to Array.pop()/.shift()
+  
+  * .join()
+    - creates and returns new string by concatenating all elements of array
+      -- separated by string method inside parenthesis (',', ', ', ' '), default is comma with no spaces (empty parenthesis)
+  
+  *.includes()
+    - returns boolean if value specified inside parenthesis is in the array or not
+
+  *.indexOf()
+    - returns index of value specifed inside parenthesis
+      -- If not in array, returns -1
+  
+  *
+*/
